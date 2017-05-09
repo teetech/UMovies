5a53ea5c3a08221129aeffdc99063a40b4839c32
Anthony Awuzie <anthonyawuzie@gmail.com>
2017-05-09 16:06:34 -0500

    network test succesful

--- app/src/main/java/com/infoprogrammer/umovies/MainActivity.java
+++ app/src/main/java/com/infoprogrammer/umovies/MainActivity.java
@@ -1,13 +1,90 @@
 package com.infoprogrammer.umovies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.infoprogrammer.umovies.Util.Utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HttpURLConnection connection = null;
+    private BufferedReader reader = null;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button hitBt = (Button) findViewById(R.id.go_btn);
+        textView = (TextView) findViewById(R.id.result);
+
+        hitBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Task().execute(Utils.NOW_PLAYING_URL);
+            }
+        });
+
     }
+
+
+
+    public class Task extends AsyncTask<String, String , String > {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            try {
+                URL url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+                reader  = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer stringBuffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null){
+                    stringBuffer.append(line);
+                }
+
+                return  stringBuffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textView.setText(s);
+        }
+    }
+
 }
